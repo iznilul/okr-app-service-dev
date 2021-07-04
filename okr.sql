aaef2aac00dc5a340787de384a32ceeabf4112a1
iznilul <1773950094@qq.com>
2021-07-04 16:59:09 +0800

    [llz] 分支测试

--- okr.sql
+++ okr.sql
@@ -11,29 +11,36 @@
  Target Server Version : 50722
  File Encoding         : 65001
 
- Date: 10/06/2021 19:39:56
+ Date: 04/07/2021 16:44:15
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for login
+-- Table structure for user
 -- ----------------------------
-DROP TABLE IF EXISTS `login`;
-CREATE TABLE `login`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `uid` bigint(11) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `role` tinyint(11) NOT NULL COMMENT '0管理员 1老师 2学生',
   `account` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '学号',
   `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
-  `createTime` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `updateTime` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 102 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '登录表' ROW_FORMAT = Dynamic;
+  `userName` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '姓名',
+  `avatar` mediumtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '头像',
+  `major` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '专业班级',
+  `qq` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'QQ号',
+  `phone` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号',
+  `weixin` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '微信号',
+  `desc` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '学习/研究方向\r\n',
+  `createTime` bigint(11) NULL DEFAULT NULL COMMENT '创建时间',
+  `updateTime` bigint(11) NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`uid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '成员表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of login
+-- Records of user
 -- ----------------------------
-INSERT INTO `login` VALUES (100, '18111493050', '18111493050', '2021-06-05 20:43:25', '2021-06-05 20:43:28');
-INSERT INTO `login` VALUES (101, '123', '123', '2021-06-09 16:53:35', '2021-06-09 16:53:35');
+INSERT INTO `user` VALUES (1, 1, '123', '202cb962ac59075b964b07152d234b70', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1625387003758, 1625387003758);
 
 SET FOREIGN_KEY_CHECKS = 1;
