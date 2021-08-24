aebaf940218ded55c5580f7cc2540dadadc79775
iznilul <1773950094@qq.com>
2021-08-25 00:15:32 +0800

    集成redis

---
+++ src/main/java/com/softlab/okr/config/LettuceRedisConfig.java
@@ -0,0 +1,30 @@
+package com.softlab.okr.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
+
+/**
+ * @program: okr
+ * @description:
+ * @author: radCircle
+ * @create: 2021-08-24 22:03
+ **/
+@Configuration
+public class LettuceRedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Serializable> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
+
---
+++ src/main/test/com/softlab/okr/RedisTest.java
@@ -0,0 +1,59 @@
+package com.softlab.okr;
+
+import com.softlab.okr.model.entity.Resource;
+import com.softlab.okr.security.MySecurityMetadataSource;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Set;
+
+/**
+ * @program: okr
+ * @description:
+ * @author: radCircle
+ * @create: 2021-07-10 21:44
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisTest {
+
+    @Autowired
+    RedisTemplate redisTemplate;
+
+    @BeforeClass // 公开表态无返回值
+    public static void beforeClass() throws Exception {
+        // 每次测试类执行前执行一次，主要用来初使化公共资源等
+    }
+
+    @AfterClass // 公开表态无返回值
+    public static void afterClass() throws Exception {
+        // 每次测试类执行完成后执行一次，主要用来释放资源或清理工作
+    }
+
+    @Before
+    public void setup() throws Exception {
+        // 每个测试案例执行前都会执行一次
+    }
+
+    @After
+    public void teardown() throws Exception {
+        // 每个测试案例执行完成后都会执行一次
+    }
+
+    @Test
+    public void redisTest() {
+        redisTemplate.delete("resource");
+        System.out.println(redisTemplate.opsForSet().size("resource"));
+
+        Set<Resource> set = MySecurityMetadataSource.getRESOURCES();
+        set.forEach(
+                resource -> {
+                    redisTemplate.opsForSet().add("resource", resource);
+                });
+        System.out.println(redisTemplate.opsForSet().size("resource"));
+    }
+}
